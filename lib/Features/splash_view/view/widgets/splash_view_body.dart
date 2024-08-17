d7b1a2aab8cad05031da66a28054a851a2edfe65
yousefmohamed35 <yousefmo2021@gmail.com>
2024-08-17 18:44:09 +0300

    finish splash screen

--- lib/Features/splash_view/view/widgets/splash_view_body.dart
+++ lib/Features/splash_view/view/widgets/splash_view_body.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:tic_tac_app/Features/splash_view/view/widgets/sliding_text.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +37,31 @@ class SplashViewBody extends StatelessWidget {
           aspectRatio: 1.5,
           child: Image.asset('assets/images/logo.png'),
         ),
-      const  SizedBox(height: 20),
-      const  Text(
-          'Tic Tac Toe',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
+        const SizedBox(height: 20),
+       SlidingText(slidingAnimation: slidingAnimation)
       ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        GoRouter.of(context).push('/home');
+      },
     );
   }
 }
